562712630cc0816c002f1981b096b67e7c694263
Jurgen Hahn <mail.jhahn@googlemail.com>
2023-05-17 22:22:43 +0200

    clean up python and rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,25 @@
-use pyo3::prelude::*;
-use pyo3::{Python, PyErr};
 use pyo3::exceptions;
+use pyo3::prelude::*;
+use pyo3::Python;
 use std::str;
 use std::time::Duration;
 use std::time::SystemTime;
-// Example:
-// https://saidvandeklundert.net/2021-11-18-calling-rust-from-python-using-pyo3/
+
 #[pyclass]
 struct PySerial {
     serial: Box<dyn serialport::SerialPort>,
 }
 
-
-// impl std::convert::From<serialport::Error> for PyErr {
-//     fn from(err: serialport::Error) -> PyErr {
-//         exceptions::PyOSError::py_err(err.to_string())
-//     }
-// }
-
-fn check_python() -> PyResult<()> {
-Python::with_gil(|py| -> PyResult<()> {
-    py.check_signals() 
-})
+fn check_python_signals() -> PyResult<()> {
+    Python::with_gil(|py| -> PyResult<()> { py.check_signals() })
 }
 
 #[pymethods]
 impl PySerial {
     fn write(&mut self, data: &[u8]) -> PyResult<usize> {
-       Ok(self.serial.write(data)?)
+        Ok(self.serial.write(data)?)
     }
-    
+
     fn close(&mut self) {}
 
     #[new]
@@ -37,27 +27,25 @@ impl PySerial {
         // fn connect(baud_rate: u32, port: &str) -> PyResult<Box<dyn serialport::SerialPort>> {
         match serialport::new(port, baud_rate)
             .timeout(Duration::from_millis(10))
-            .open() {
-            Ok(serial) => {
-               Ok(PySerial { serial })
-            },
-            Err(error) => {
-                 Err(exceptions::PyTypeError::new_err(format!("Failed to open port {port}: {error}")))
-            }
+            .open()
+        {
+            Ok(serial) => Ok(PySerial { serial }),
+            Err(error) => Err(exceptions::PyTypeError::new_err(format!(
+                "Failed to open port {port}: {error}"
+            ))),
         }
-
     }
 
     fn read_line(&mut self, timeout_in_millis: u64) -> PyResult<Vec<char>> {
         let mut serial_buf: Vec<char> = Vec::new();
 
         let mut done = false;
-        
+
         let time_start = SystemTime::now();
-        
+
         while !done {
-            check_python()?;
-            
+            check_python_signals()?;
+
             let mut buf: Vec<u8> = vec![0, 32];
 
             match self.serial.read(buf.as_mut_slice()) {
@@ -65,7 +53,7 @@ impl PySerial {
                     for val in buf.iter() {
                         let v = *val as char;
                         serial_buf.push(v);
-                        if '\n' == v || '\r' == v  || 'a' == v {
+                        if '\n' == v || '\r' == v || 'a' == v {
                             done = true;
                             break;
                         }
@@ -73,17 +61,17 @@ impl PySerial {
                 }
                 Err(_) => {}
             };
-                      
+
             match time_start.elapsed() {
                 Ok(time_delta) => {
-                    if time_delta > Duration::from_millis(timeout_in_millis) {                        
+                    if time_delta > Duration::from_millis(timeout_in_millis) {
                         println!("Timeout occured.");
                         break;
                     }
                 }
                 Err(_) => {}
             };
-        };
+        }
 
         Ok(serial_buf)
     }
@@ -98,32 +86,9 @@ fn list_ports() {
     }
 }
 
-/// A Python module implemented in Rust.
 #[pymodule]
 fn py_rust(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(list_ports, m)?)?;
     m.add_class::<PySerial>()?;
     Ok(())
 }
-
-// fn main() {
-// list_ports(:);
-
-// let mut serial = PySerial::connect(460_800, "/dev/ttyS0");
-
-// let start = Instant::now();
-
-// while true {
-// let elapsed = start.elapsed();
-
-// match port {
-// Ok(v) => {
-// let buffer = serial.read_line();
-
-// println!("{}", elapsed.as_millis());
-// }
-// Err(e) => println!("Error {e:?}",),
-// }
-//println!("{} -> {:?}", elapsed.as_millis(), buffer);
-// }
-// }
